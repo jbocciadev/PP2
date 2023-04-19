c4945699a79cc5e6c537c26dc19bb47c8f0ed3bc
jbocciadev <>
2023-04-19 12:12:57 +0000

    Implement team name auto update on change at top level

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,26 +10,41 @@
  * Waits for document to load and assigns event listeners and actions
  */
 document.addEventListener("DOMContentLoaded", function () {
-        let buttons = document.getElementsByTagName("button");
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            let team = this.getAttribute("team");
-            let points = parseInt(this.getAttribute("points"));
-            let t = team[0];
-            assignPoints(t,points);
-        })
-    }
-})        
+  let buttons = document.getElementsByTagName("button");
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      let team = this.getAttribute("team");
+      let points = parseInt(this.getAttribute("points"));
+      let t = team[0];
+      assignPoints(t, points);
+    });
+  }
+});
 
-
+document.addEventListener("DOMContentLoaded", function () {
+  let names = document.getElementsByClassName("team-name");
+  for (let name of names) {
+    name.addEventListener("change", function () {
+      let team = this.getAttribute("team");
+      let newName = this.value;
+      if (team === "home") {
+        let hTableHeader = document.getElementById("htable-header");
+        hTableHeader.textContent = newName;
+      } else {
+        let vTableHeader = document.getElementById("vtable-header");
+        vTableHeader.textContent = newName;
+      }
+    });
+  }
+});
 
 /**
  * Retrieves current score and adds points accordingly
  */
 function assignPoints(team, points) {
-    let elementId = team + "team-points";
-    let score = document.getElementById(elementId);
-    let scorepoints = parseInt(score.textContent);
-    let newScore = scorepoints + points;
-    score.textContent = newScore;
+  let elementId = team + "team-points";
+  let score = document.getElementById(elementId);
+  let scorepoints = parseInt(score.textContent);
+  let newScore = scorepoints + points;
+  score.textContent = newScore;
 }
