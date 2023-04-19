6615585b19d30c8f7d0b9fdcb45a1d1a642b46d4
jbocciadev <>
2023-04-19 11:47:30 +0000

    Simplify main function

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,31 +10,17 @@
  * Waits for document to load and assigns event listeners and actions
  */
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button");
-for (let button of buttons) {
-    button.addEventListener("click", function() {
-        switch (this.getAttribute("data-type")) {
-            case "h1":
-                assignPoints("h", 1);
-                break;
-            case "h2":
-                assignPoints("h", 2);
-                break;
-            case "h3":
-                assignPoints("h", 3);
-                break;
-            case "v1":
-                assignPoints("v", 1);
-                break;
-            case "v2":
-                assignPoints("v", 2);
-                break;
-            case "v3":
-                assignPoints("v", 3);
-        }
-    })
-}
-})
+        let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            let team = this.getAttribute("team");
+            let points = parseInt(this.getAttribute("points"));
+            let t = team[0];
+            assignPoints(t,points);
+        })
+    }
+})        
+
 
 
 /**
