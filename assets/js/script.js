c9c3ed1653b03594f66f6a1d3da98e3162f46351
jbocciadev <>
2023-04-26 14:23:58 +0000

    Initial attempt at loop to load entries on tables

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,10 @@ let teams = [homeTeam, guestTeam]; //Adding teams to array so it can be accessed
 // Wait for document to load and assign event listeners and actions
 //Add points based on the button clicked
 document.addEventListener("DOMContentLoaded", function () {
+  //Insert table rows
+
+  loadTables();
+
   let buttons = document.getElementsByTagName("button");
   for (let button of buttons) {
     button.addEventListener("click", function () {
@@ -55,24 +59,21 @@ document.addEventListener("DOMContentLoaded", function () {
         this.value = teams[teamIndex].lineup[playerIndex][1]; //accessing player no. from the array
       } else {
         teams[teamIndex].lineup[playerIndex][1] = n;
-      };
+      }
     });
   }
 
   //On player name change, update name in player's array
   let playerNames = document.getElementsByName("player-name");
   for (let pName of playerNames) {
-    pName.addEventListener("change", function(){
-        newName = this.value;
-        let teamIndex = parseInt(this.getAttribute("team-index"));
-        let playerIndex = parseInt(this.getAttribute("player-index"));
-        teams[teamIndex].lineup[playerIndex][2] = newName;
-    }) 
+    pName.addEventListener("change", function () {
+      newName = this.value;
+      let teamIndex = parseInt(this.getAttribute("team-index"));
+      let playerIndex = parseInt(this.getAttribute("player-index"));
+      teams[teamIndex].lineup[playerIndex][2] = newName;
+    });
   }
-
 });
-
-
 
 // Custom functions
 
@@ -94,9 +95,8 @@ function createLineup() {
  * team = 0 is the home team, team = 1 is the guest team
  */
 function addPoints(team, player, points) {
-
   //Accessing team's running points tally, testing for zero and adding new points scored.
-  // teams[team].currentScore === 'zero' ? teams[team].currentScore = points : 
+  // teams[team].currentScore === 'zero' ? teams[team].currentScore = points :
   teams[team].currentScore += points;
 
   //Adding points scored to the array of points scored by the player
@@ -123,36 +123,71 @@ function displayPoints() {
   for (let i = 0; i < 2; i++) {
     for (let j = 0; j < 13; j++) {
       playerPoints = teams[0].lineup[j][4];
-      let parent = 'player-' + i + '-' + j;
+      let parent = "player-" + i + "-" + j;
       let a = document.querySelector(`#${parent} :nth-child(3)`);
       a.textContent = playerPoints;
       console.log(a.textContent);
     }
   }
-
-
 }
 
 function checkPlayerNo(number) {
-  if (isNaN(number)) {  //check for non-number values
+  if (isNaN(number)) {
+    //check for non-number values
     alert("Please input a number between 0 and 99");
     return 1;
   } else {
-    if (number < 0 || number > 99) { //check for valid numeric value
+    if (number < 0 || number > 99) {
+      //check for valid numeric value
       alert("Please input a number between 0 and 99");
       return 1;
-    };
-  }};
+    }
+  }
+}
 
 /**
-* Displays team name on name change
-*/
+ * Displays team name on name change
+ */
 
 /**
-* Displays player name on namechange
-*/
-
+ * Displays player name on namechange
+ */
 
 //Add script to display modal(s)?
 
-
+/**
+ * Loads tables on stats
+ */
+function loadTables() {
+  let tbodies = document.getElementsByTagName("tbody");
+  for (let tb of tbodies) {
+    let team = 0;
+    tb.getAttribute("team") === "home" ? (team = 0) : (team = 1);
+    let contents = "";
+    //Iterate through 12 players to create the rows for the tables
+    for (let i = 0; i <= 12; i++) {
+      let row = `<tr id="player-${team}-${i}">
+        <td><input t="${team}" pi="${i}" name="player-no" type="text" pattern="[0-9]" maxlength="2" value="${i}"></td>
+        <td><input type="text" team-index="${team}" player-index="${i}" name="player-name" maxlength="20" placeholder="Name" value="Name"></td>
+        <td>0</td>
+        <td>
+            <div class="buttons-div">
+            <button tTeam="homeTeam" team="home" ti="${team}" points="1" player-index="${i}" class="btn-1p">
+                +1
+            </button>
+            <button tTeam="homeTeam" team="home" ti="${team}" points="2" player-index="${i}" class="btn-2p">
+                +2
+            </button>
+            <button tTeam="homeTeam" team="home" ti="${team}" points="3" player-index="${i}" class="btn-3p">
+                +3
+            </button>
+            </div>
+        </td>
+        </tr>
+        `;
+      contents += row;
+      console.log(contents);
+    }
+    tb.innerHTML = contents;
+  }
+}
